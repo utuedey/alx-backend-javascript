e4674a654a76e091c9365758a868687724fa25d6
utuedey <utuedeyejosph@gmail.com>
2023-08-22 05:32:14 +0000

    modify 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,19 @@
 /**
 * a simple http server
 */
-const app = require('http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.status_code = 200;
   res.setHeader('Content-type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
