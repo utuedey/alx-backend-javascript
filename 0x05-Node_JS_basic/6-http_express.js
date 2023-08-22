61922f145d863c8bd77a9596a337a594113c5301
utuedey <utuedeyejosph@gmail.com>
2023-08-22 05:46:11 +0000

    create 6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+/**
+* a simple express server
+*/
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
